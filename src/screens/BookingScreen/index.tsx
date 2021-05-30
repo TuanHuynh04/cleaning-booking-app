3a98b995674f539c703437220fec5b15bbc33b1a
Huỳnh Văn Phước <47907938+phuocvn@users.noreply.github.com>
2021-05-30 09:31:58 +0700

    man hinh login

--- src/screens/BookingScreen/index.tsx
+++ src/screens/BookingScreen/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Button, Checkbox } from "@material-ui/core";
+import React from "react";
+import styled from "styled-components";
+
+const BookingScreenStyles = styled.div`
+  h1 {
+    color: red;
+  }
+`;
 
 export const BookingScreen = () => {
-    return (
-        <div>
-            Booking Screen
-        </div>
-    )
-}
+    const [checked, setChecked] = React.useState(true);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setChecked(event.target.checked);
+    };
+  
+  return (
+    <BookingScreenStyles>
+      <h1>Booking Screen</h1>
+      <Checkbox
+        checked={checked}
+        onChange={handleChange}
+        inputProps={{ 'aria-label': 'primary checkbox' }}
+      />
+      <Button variant="contained" color="secondary">
+        Hằng Tháng
+      </Button>
+    </BookingScreenStyles>
+  );
+};
--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export const LoginScreen = () => {
-    return (
-        <div>
-            Login Screen
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Login Screen</h1>
+    </div>
+  );
+};
